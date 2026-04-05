7cb5cace1f01266ee3cded40a42ab36fa09f1a7b
DevZenthor <zenthorpro@outlook.com>
2026-04-05 23:25:30 +0200

    feat : twitch session

--- marcok/src/components/Twitch.jsx
+++ marcok/src/components/Twitch.jsx
@@ -1,17 +1,46 @@
+import { useEffect, useState } from "react";
+
 function Twitch() {
+  const [isLive, setIsLive] = useState(false);
+
+  useEffect(() => {
+    const fakeStatus = true; // change true / false pour tester
+    setIsLive(fakeStatus);
+  }, []);
+
   return (
     <div id="twitch" className="container py-5 text-center">
 
-      <h2 className="section-title">🔴 LIVE NOW</h2>
+      <h2 className="live-wrapper">
+  {isLive ? (
+    <>
+      <span className="live-dot"></span>
+      LIVE
+    </>
+  ) : (
+    "OFFLINE"
+  )}
+</h2>
 
-      <div className="twitch-box mt-4">
-        <iframe
-          src="https://player.twitch.tv/?channel=tonpseudo&parent=localhost"
-          height="450"
-          width="100%"
-          allowFullScreen>
-        </iframe>
-      </div>
+      {isLive ? (
+        <div className="twitch-box mt-4">
+          <iframe
+            src="https://player.twitch.tv/?channel=tonpseudo&parent=localhost"
+            height="450"
+            width="100%"
+            allowFullScreen>
+          </iframe>
+        </div>
+      ) : (
+           <div className="twitch-box mt-4">
+          <iframe
+            src="https://player.twitch.tv/?channel=tonpseudo&parent=localhost"
+            height="450"
+            width="100%"
+            allowFullScreen>
+          </iframe>
+        </div>
+      )}
 
     </div>
   );
